99e6ea5e83128e10c2e36810f8557eba64c0094e
Derick Ostrenko <dostrenko@lsu.edu>
2024-11-13 13:20:46 -0600

    Refactor particle rendering and background color in sketch.js

--- sketch.js
+++ sketch.js
@@ -5,43 +5,30 @@ function setup() {
 }
 
 function draw() {
-  background(200);
-
-  // Create new particles when mouse is pressed
-  if (mouseIsPressed) {
+  background(220);
+  
+  if(mouseIsPressed) {
     particles.push(new Particle(mouseX, mouseY));
   }
 
-  // Update and show all particles
-  for (let i = particles.length - 1; i >= 0; i--) { // Looping backwards to safely remove particles
+  for(let i = 0; i < particles.length; i++) {
     particles[i].display();
-
-    // Remove particles that move off screen
-    if (particles[i].x < 0 || particles[i].x > width || particles[i].y < 0 || particles[i].y > height) {
-      particles.splice(i, 1);
-    }
   }
+
 }
 
 class Particle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-
-    // Random speed for each particle
     this.speedX = random(-3, 3);
     this.speedY = random(-3, 3);
   }
 
   display() {
-    
-    // Draw the particle as a black circle
-    noStroke();
-    fill(0);
-    ellipse(this.x, this.y, 10);
-
-    // Update the particle's position
+    ellipse(this.x, this.y, 10, 10);
     this.x += this.speedX;
     this.y += this.speedY;
   }
+
 }
